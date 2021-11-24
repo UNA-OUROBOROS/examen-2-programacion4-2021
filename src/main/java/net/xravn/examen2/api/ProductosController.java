e20465f93de3abc2d064baa71dd473113aab22bc
Jonathan Guzmán <jonguz5@hotmail.com>
2021-11-24 16:01:08 -0600

    actualizar producto

--- src/main/java/net/xravn/examen2/api/ProductosController.java
+++ src/main/java/net/xravn/examen2/api/ProductosController.java
@@ -5,28 +5,27 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import net.xravn.examen2.controller.GeneralController;
 import net.xravn.examen2.model.Producto;
 
-@Controller
+@RestController
+@RequestMapping("api")
 public class ProductosController {
-    
-    @GetMapping("api/productos")
-    @ResponseBody
+
+    @GetMapping("/productos")
     public List<Producto> getProductos() {
         return GeneralController.getInstance().getProductos();
     }
 
-
-    @GetMapping("api/productos/{id}")
-    @ResponseBody
+    @GetMapping("/productos/{id}")
     public ResponseEntity<Producto> getProducto(@PathVariable Integer id) {
         Producto producto = GeneralController.getInstance().getProducto(id);
         if (producto == null) {
@@ -35,8 +34,7 @@ public class ProductosController {
         return ResponseEntity.ok(producto);
     }
 
-    @PostMapping("api/productos")
-    @ResponseBody
+    @PostMapping("/productos")
     // retorna una tupla con el estado y el mensaje
     public ResponseEntity<Map<String, Object>> postProducto(@RequestBody Producto producto) {
         Map<String, Object> respuesta = new HashMap<>();
@@ -44,28 +42,58 @@ public class ProductosController {
         if (producto.getNombre() == null || producto.getNombre().isEmpty()) {
             respuesta.put("exitoso", false);
             respuesta.put("mensaje", "El nombre del producto es obligatorio");
-        }
-        else if (producto.getPrecio() == null || producto.getPrecio() < 0) {
+        } else if (producto.getPrecio() == null || producto.getPrecio() < 0) {
             respuesta.put("exitoso", false);
             respuesta.put("mensaje", "El precio del producto no es valido");
-        }
-        else if (producto.getStock() == null|| producto.getStock() < 0) {
+        } else if (producto.getStock() == null || producto.getStock() < 0) {
             respuesta.put("exitoso", false);
             respuesta.put("mensaje", "el stock del producto no es valido");
-        }
-        else if(GeneralController.getInstance().agregarProducto(producto)) {
+        } else if (GeneralController.getInstance().agregarProducto(producto)) {
             respuesta.put("exitoso", true);
             respuesta.put("mensaje", "Producto agregado");
-        }
-        else {
+        } else {
             respuesta.put("exitoso", false);
             respuesta.put("mensaje", "error desconocido al agregar el producto");
         }
         if (respuesta.get("exitoso").equals(true)) {
             return ResponseEntity.ok(respuesta);
-        }
-        else {
+        } else {
             return ResponseEntity.unprocessableEntity().body(respuesta);
         }
     }
+
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Map<String, Object>> actualizarProducto(@PathVariable Integer id,
+            @RequestBody Producto producto) {
+        Map<String, Object> respuesta = new HashMap<>();
+        Producto productoActual = GeneralController.getInstance().getProducto(id);
+
+        if (productoActual == null) {
+            respuesta.put("exitoso", false);
+            respuesta.put("mensaje", "El producto no existe");
+        } else if (producto.getNombre() == null || producto.getNombre().isEmpty()) {
+            respuesta.put("exitoso", false);
+            respuesta.put("mensaje", "El nombre del producto es obligatorio");
+        } else if (producto.getPrecio() == null || producto.getPrecio() < 0) {
+            respuesta.put("exitoso", false);
+            respuesta.put("mensaje", "El precio del producto no es valido");
+        } else {
+            producto.setId(productoActual.getId());
+            producto.setStock(productoActual.getStock());
+            if (GeneralController.getInstance().actualizarProducto(producto)) {
+                respuesta.put("exitoso", true);
+                respuesta.put("mensaje", "Producto actualizado");
+            } else {
+                respuesta.put("exitoso", false);
+                respuesta.put("mensaje", "error desconocido al actualizar el producto");
+            }
+        }
+
+        if (respuesta.get("exitoso").equals(true)) {
+            return ResponseEntity.ok(respuesta);
+        } else {
+            return ResponseEntity.unprocessableEntity().body(respuesta);
+        }
+    }
+
 }
